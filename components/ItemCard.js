3192c62500cd2e75a6656b79e2370d95d40cb7b2
Neele Wähnke <neele.waehnke@hotmail.de>
2022-12-13 15:04:49 +0100

    Change styled components to better practice version

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -3,13 +3,11 @@ import styled from 'styled-components';
 export default function ItemCard({ name, date, quantity }) {
   return (
     <StyledItem>
-      <h3>{name}</h3>
-
-      <p className="qty">Qty:</p>
-      <div className="qty-value">{quantity}</div>
-
-      <p className="date">date:</p>
-      <div className="date-value">{date}</div>
+      <StyledName>{name}</StyledName>
+      <StyledQty>Qty:</StyledQty>
+      <StyledQtyValue>{quantity}</StyledQtyValue>
+      <StyledDate>date:</StyledDate>
+      <StyledDateValue>{date}</StyledDateValue>
     </StyledItem>
   );
 }
@@ -24,35 +22,42 @@ const StyledItem = styled.li`
   grid-template-rows: auto;
   grid-template-areas: 'name . .' 'name qty date' 'name qtyvalue datevalue  ';
   padding: 15px;
-  h3,
-  p,
-  div {
-    margin: 2%;
-    display: grid;
-  }
-  .date {
-    grid-area: date;
-    justify-self: center;
-    align-self: center;
-  }
-  .date-value {
-    grid-area: datevalue;
-    justify-self: center;
-    align-self: center;
-  }
-  .qty {
-    grid-area: qty;
-    justify-self: center;
-    align-self: center;
-  }
-  .qty-value {
-    grid-area: qtyvalue;
-    justify-self: center;
-    align-self: center;
-  }
-  h3 {
-    justify-self: start;
-    align-self: center;
-    grid-area: name;
-  }
+`;
+const StyledName = styled.h3`
+  margin: 2%;
+  justify-self: start;
+  align-self: center;
+  grid-area: name;
+  display: grid;
+`;
+
+const StyledDate = styled.p`
+  margin: 2%;
+  grid-area: date;
+  justify-self: center;
+  align-self: center;
+  display: grid;
+`;
+
+const StyledQty = styled.p`
+  margin: 2%;
+  display: grid;
+  grid-area: qty;
+  justify-self: center;
+  align-self: center;
+`;
+
+const StyledQtyValue = styled.div`
+  margin: 2%;
+  display: grid;
+  grid-area: qtyvalue;
+  justify-self: center;
+  align-self: center;
+`;
+const StyledDateValue = styled.div`
+  margin: 2%;
+  display: grid;
+  grid-area: datevalue;
+  justify-self: center;
+  align-self: center;
 `;
