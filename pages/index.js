853d42c7a60b5a06b7ea50a07c8a701c0b59e24d
Neele Wähnke <neele.waehnke@hotmail.de>
2022-12-21 17:16:45 +0100

    save

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Storages from '../components/Storages';
 import Header from '../components/Header';
 import useLocalStorageState from 'use-local-storage-state';
-import { dummyitems } from '../lib/data';
+
 
 export default function Home({ storages, setStorages }) {
   const [items, setItems] = useLocalStorageState('items', {
