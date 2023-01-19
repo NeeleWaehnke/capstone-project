c5d8d8bbf6a1c00fb266b97a150a069a00e44c58
Neele Wähnke <neele.waehnke@hotmail.de>
2023-01-19 12:04:56 +0100

    Add folder for container styles

---
+++ components/Container/ContainerStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const HomepageContainer = styled.div`
+  @media (min-height: 700px) {
+    margin-top: 170px;
+  }
+  @media (min-width: 1000px) {
+    margin-top: 180px;
+  }
+
+  margin-top: 150px;
+  margin-bottom: 100px;
+`;
+
+export const PagesContainer = styled.div`
+  margin-bottom: 100px;
+  margin-top: 120px;
+`;
