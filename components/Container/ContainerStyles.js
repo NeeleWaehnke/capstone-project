29bf1a2d2863cfe60892395a1b133dcb1b7d360c
Neele Wähnke <neele.waehnke@hotmail.de>
2023-01-19 12:41:14 +0100

    update styling of container to fit new styling of header

--- components/Container/ContainerStyles.js
+++ components/Container/ContainerStyles.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 
 export const HomepageContainer = styled.div`
   @media (min-height: 700px) {
-    margin-top: 170px;
-  }
-  @media (min-width: 1000px) {
     margin-top: 180px;
   }
+  @media (min-width: 1000px) {
+    margin-top: 190px;
+  }
 
-  margin-top: 150px;
+  margin-top: 160px;
   margin-bottom: 100px;
 `;
 
